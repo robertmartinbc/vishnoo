e428fa5bb321528ac80510ac6a3f7f1c8e73dc2f
Robert Martin <robertmartinbc@shaw.ca>
2013-12-29 15:06:17 -0800

    First commit and README update.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Vishnoo::Application.config.secret_token = '41390c4cd80138cdca41479f0bbbc3806b16584bc3060cde127ddc4468231ff1ede5af251e0541f999267bbe0408031c3e5903618d5cc5765220f52b8e07c353'
