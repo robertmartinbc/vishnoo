4f36d01cfaa5313c74702dbeef02f8b3eb28b89d
Robert Martin <robertmartinbc@shaw.ca>
2013-12-29 23:24:26 -0800

    Added Pagination.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,11 @@
 class PostsController < ApplicationController
   def index
-  	@posts = Post.all
+  	@posts = Post.paginate(page: params[:page], per_page: 10)
   end
 
   def show
   	@post = Post.find(params[:id])
+    @posts = @post.paginate(page: params[:page], per_page: 10)
   end
 
   def new
