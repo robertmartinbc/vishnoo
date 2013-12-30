ef4a7236c553153e54ac1efb9601a93ed7d0b12a
Robert Martin <robertmartinbc@shaw.ca>
2013-12-29 23:42:11 -0800

    Trying to fix pagination.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,7 +5,7 @@ class PostsController < ApplicationController
 
   def show
   	@post = Post.find(params[:id])
-    @posts = @post.paginate(page: params[:page], per_page: 10)
+    @posts = Post.paginate(page: params[:page], per_page: 10)
   end
 
   def new
